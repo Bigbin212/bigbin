164bc3648ac880bc0bda3b28b57bdbdfca4ab807
liubin_znjt <liubin_znjt@kedacom.com>
2019-07-19 14:44:58 +0800

    提交测试

--- src/main/java/com/bigbincome/bigbin/controller/TestController.java
+++ src/main/java/com/bigbincome/bigbin/controller/TestController.java
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * 这里的@RestController   相当于@ResponseBody + @Controller
+ */
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
 
     @Autowired
     TestService testService;
 
-    @RequestMapping(value = "/test/index",method = RequestMethod.GET)
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
     public String test(){
         return "卜憨憨";
     }
 
-    @RequestMapping(value = "/test/search",method = RequestMethod.GET)
+    @RequestMapping(value = "/search",method = RequestMethod.GET)
     public Object searchForm(@RequestParam("username") String username){
         JSONObject jsonObject = new JSONObject();
         List<BZUserEntity> list = testService.findByUserName(username);
